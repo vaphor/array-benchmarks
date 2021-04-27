288c6e5e4126b0eb74923069a9ab07d15439476a
BRAINE Julien <julien.braine@inria.fr>
2021-04-28 00:44:50 +0200

    cd before checkout

--- install_all.sh
+++ install_all.sh
@@ -6,26 +6,26 @@ git checkout $tag
 cd ..
 echo "Cloning and checking out to $tag commit of Vaphor Tool"
 git clone "git@github.com:vaphor/CellMorphing.git"
+cd "CellMorphing"
 git checkout $tag
 echo "Making Vaphor executable"
-cd "CellMorphing"
 make
 cd ..
 
 echo "Cloning and checking out to $tag commit of Converter Tool"
 git clone "https://github.com/vaphor/hornconverter.git"
+cd "hornconverter"
 git checkout $tag
 echo "Making Converter executable"
-cd "hornconverter"
 make
 cd ..
 
 
 echo "Cloning and checking out to $tag commit of DataAbstraction Tool"
 git clone "https://github.com/vaphor/DataAbstraction.git"
+cd "DataAbstraction"
 git checkout $tag
 echo "Making DataAbstraction executable"
-cd "DataAbstraction"
 make
 cd ..
 
