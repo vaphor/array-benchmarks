08681ff80d5a91fb1097cc73b38d4a3a5996894f
BRAINE Julien <julien.braine@inria.fr>
2021-05-02 20:54:58 +0200

    sql analysis

--- src/analysis.sql
+++ src/analysis.sql
@@ -1,4 +1,3 @@
--- .param set :resfile %1
 .mode csv
 .separator ;
 .import !resfile! Bench
@@ -26,7 +25,11 @@ SELECT file, (CASE
              (CASE 
               WHEN abs like '%cell1%' THEN 1
               WHEN abs like '%cell2%' THEN 2 
-              ELSE null END) as n,
+              ELSE 'NA' END) as n,
+             (CASE 
+              WHEN solver like '%z3%' THEN 'Z3'
+              ELSE 'other' END) as solvertool,
+             solver as solveropt,
              (CASE
                 WHEN (((file like '%buggy%') and result="unsat") or (not (file like '%buggy%') and result="sat")) THEN 'correct'
                 WHEN (((file like '%buggy%') and result="sat") or (not (file like '%buggy%') and result="unsat")) THEN 'incorrect'
@@ -37,34 +40,22 @@ SELECT file, (CASE
 FROM Bench
 WHERE not (file like '%buggy%' and preprocess like '%hinted%');
 
-CREATE VIEW rmpn AS
-SELECT file, ftype, n, abstool, result, SUM(result=result) as 'nbexp', MAX(result=result) as 'nbexists' FROM nice
-GROUP BY file, ftype, n, abstool, result;
+CREATE VIEW nbexp AS
+SELECT file, ftype, n, abstool, solvertool, COUNT(*) as 'nbexp' FROM nice
+GROUP BY file, ftype, n, abstool, solvertool;
 
+CREATE VIEW nbfiles AS
+SELECT ftype, COUNT(*) as nbf FROM (SELECT DISTINCT ftype, file FROM nice)
+GROUP BY ftype;
+
+
+CREATE VIEW rmpn AS
+SELECT file, ftype, n, abstool, solvertool, result, ROUND((COUNT(*) * 1.0)/(CAST(nbexp AS REAL)), 2) as avgexp, MAX(result=result) as nbexists FROM nice NATURAL JOIN nbexp
+GROUP BY file, ftype, n, abstool, solvertool, result;
 
 CREATE VIEW final AS
-SELECT ftype, n, abstool, ('#' || result) as resulttype, SUM(nbexp) as res FROM rmpn 
-GROUP BY ftype, n, abstool, result
+SELECT ftype, n, abstool, solvertool, result, SUM(avgexp) as avgexp, SUM(nbexists) as nbexists FROM rmpn
+GROUP BY ftype, n, abstool, solvertool, result;
 
-UNION
-
-SELECT ftype, n, abstool, ('>=' || result) as resulttype, SUM(nbexists) as res FROM rmpn 
-GROUP BY ftype, n, abstool, result;
-
-SELECT ftype, COALESCE(n, 'NA') as n, abstool, resulttype, COALESCE(res,0) as res FROM final
-
--- SELECT (ftype || ',' || COALESCE(n, 'NA')) as 'ftype,n', abstool || resulttype, COALESCE(res,0) as res FROM final
-
--- CREATE VIEW sumcorrect AS
--- SELECT file, ftype, n, abstool, result, Count() FROM nice
--- GROUP BY file, ftype, n, abstool, result;
--- 
--- 
--- SELECT abstool, result, Count(*) FROM nice
--- WHERE n=1 or n is null
--- GROUP BY abstool, result
-
-
-
--- SELECT * FROM Files LIMIT 10
-   
+SELECT ftype, nbf as '#prg', COALESCE(n, 'NA') as n, abstool, solvertool, result, COALESCE(avgexp,0) as avgexp, COALESCE(nbexists,0) as nbexists FROM final NATURAL JOIN nbfiles
+ORDER BY ftype, n, abstool, result
